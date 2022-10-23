1adbebc223d0d16b2dce25bc2e53a59c6ec216d9
Adam Dia <43157907+dooplu@users.noreply.github.com>
2022-10-23 17:39:29 -0400

    Update findIRCamerawithWinSDK.py

--- code/tests/findIRCamerawithWinSDK.py
+++ code/tests/findIRCamerawithWinSDK.py
@@ -2,6 +2,8 @@ import asyncio
 import winsdk.windows.media.capture as wdmc
 import winsdk.windows.media.capture.frames as wdmcf
 import winsdk.windows.media.mediaproperties as wmmp
+import cv2 as cv
+from PIL import Image
 
 async def getCameras():
     frameSourceGroups = await wdmcf.MediaFrameSourceGroup.find_all_async()
@@ -38,11 +40,28 @@ except:
 
 colorFrameSource = mediaCapture.frame_sources[colorSourceInfo.id]
 for format in colorFrameSource.supported_formats:
-    if format.video_format.width >= 0 and format.subtype == wmmp.MediaEncodingSubtypes.argb32:
+    if format.video_format.width >= 720 and format.subtype == 'argb32':
         break
 preferredFormat = format
+print(preferredFormat)
 
 async def setFormat():
     await colorFrameSource.set_format_async(preferredFormat)
 
 asyncio.run(setFormat())
+
+
+async def getFrames():
+    mediaFrameReader = await mediaCapture.create_frame_reader_async(colorFrameSource, 'argb32')
+    mediaFrameReader.try_acquire_latest_frame()
+    await mediaFrameReader.start_async()
+    return mediaFrameReader
+    
+
+mediaFrameReader = asyncio.run(getFrames())
+
+img = mediaFrameReader.
+
+#open("C:/Users/Adam/Desktop/img.txt", "wb").write(img)
+
+#cv.imshow('Camera', img)
