af6b914341f4904c261cdda4c7eec97dc5af039e
uoRetr0 <114036712+uoRetr0@users.noreply.github.com>
2022-12-24 16:06:23 -0500

    fps to main

--- src/main.py
+++ src/main.py
@@ -2,9 +2,14 @@ import widgets
 import gestureRecognition
 import cv2 as cv
 import numpy as np
+import time
 
 screenWidth, screenHeight = 720, 480
 outputImage = np.zeros((screenHeight, screenWidth, 3), np.uint8)
+
+font = cv.FONT_HERSHEY_SIMPLEX
+
+previousTime = 0
 
 
 
@@ -19,6 +24,13 @@ while True:
     if flag == 0:
         break
 
+    
+    fps = 1/(time.time()-previousTime)
+    fps = int(fps)
+    fps = str(fps)
+    previousTime = time.time()
+    
+    cv.putText(debugImage, fps, (5,35), font, 1,(255,255,255),2,cv.LINE_AA)
     cv.imshow('debug', debugImage)
     cv.imshow('output', outputImage)
 
