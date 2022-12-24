fdefe1adff9534052c9ed48567ee1d888f542261
Adam Dia <43157907+dooplu@users.noreply.github.com>
2022-12-23 23:28:02 -0500

    ui basics

---
+++ src/widgets.py
@@ -0,0 +1,21 @@
+import cv2 as cv
+import numpy as np
+
+class object:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+
+class circle(object):
+    def __init__(self, x, y, radius, colour):
+        super().__init__(x, y)
+        self.radius = radius
+        self.colour = colour
+
+    def display(self, image, mouseX, mouseY):
+        cv.circle(image, (self.x, self.y), self.radius, self.colour, -1, cv.LINE_AA)
+        #return self.img
+
+
+
