4548c103254cd7d86d908e40cba8b6507a772063
Adam Dia <43157907+dooplu@users.noreply.github.com>
2022-12-24 19:36:32 -0500

    created lerp and smoothedGesture

--- src/main.py
+++ src/main.py
@@ -3,9 +3,15 @@ import gestureRecognition
 import cv2 as cv
 import numpy as np
 import time
+from collections import deque
 
 screenWidth, screenHeight = 720, 480
 outputImage = np.zeros((screenHeight, screenWidth, 3), np.uint8)
+#smoothedImage = np.zeros((screenHeight, screenWidth, 3), np.uint8)
+gestureHistory = deque(maxlen=10)
+smoothGestureThreshold = 0.5
+
+glob = widgets.circle(0, 0, 50, (255, 255, 255))
 
 def fps(image, previousTime):
     font = cv.FONT_HERSHEY_SIMPLEX
@@ -16,11 +22,45 @@ def fps(image, previousTime):
 
 # organize all the drawing into its own function
 def draw(image, landmarks, gesture):
-    if gesture == 2:
-        cv.circle(image, (landmarks[8][0], landmarks[8][1]), 10, (255,0,0), -1, cv.LINE_AA)
-    if gesture == 3:
-        image = np.zeros((screenHeight, screenWidth, 3), np.uint8)
+    if len(landmarks) < 1:
+        return image
+    # clear the frame
+    image = np.zeros((screenHeight, screenWidth, 3), np.uint8)
+
+    if gesture == 1:
+        glob.x = int(lerp(glob.x, landmarks[9][0], 0.20))
+        glob.y = int(lerp(glob.y, landmarks[9][1], 0.20))
+
+    glob.display(image)
     return image
+
+# linearly interpolate between two values
+def lerp(starting, ending, percentage):
+    return starting + (ending - starting) * percentage
+
+# to smooth out noise and avoid sudden changes due to false positives
+def smoothGesture(currentGesture, gestureHistory, smoothGestureThreshold):
+    if len(gestureHistory) < 1:
+        return 0
+    smoothed = currentGesture
+    counter = 0
+    gesturePresences = {}
+    for gesture in gestureHistory:
+        if gesture not in gesturePresences:
+            gesturePresences[gesture] = 0      
+        gesturePresences[gesture]  = gesturePresences[gesture] + 1
+    
+    currentGesturePresence = gesturePresences.get(currentGesture, 0) / len(gestureHistory) 
+
+    if currentGesturePresence >= smoothGestureThreshold:
+        smoothed = currentGesture
+    else:
+        for key in gesturePresences:
+            if gesturePresences[key] == max(gesturePresences.values()):
+                smoothed = key
+                break
+
+    return smoothed
 
 # initialize the hand tracking and gesture recognition
 cap, hands, point_history, keypoint_classifier, point_history_classifier, history_length, finger_gesture_history = gestureRecognition.init(1)
@@ -37,10 +77,15 @@ while True:
     if flag == 0:
         break
 
+    smoothedGesture = smoothGesture(currentGesture, gestureHistory, smoothGestureThreshold)
     
-    outputImage = draw(outputImage, landmarks, currentGesture)
-    fps(debugImage, previousTime)
+    outputImage = draw(outputImage, landmarks, smoothedGesture)
+    #smoothedImage = draw(smoothedImage, landmarks, smoothedGesture)
 
+    fps(debugImage, previousTime)
+    gestureHistory.append(currentGesture)
+
+    #cv.imshow('smoothed', smoothedImage)
     cv.imshow('debug', debugImage)
     cv.imshow('output', outputImage)
 
